5bee97efd4622f0d13577ddb77e395bf177aab9d
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-06-19 12:16:44 +0300

    css link

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ let days = [
   "WEDNESDAY",
   "THURSDAY",
   "FRIDAY",
-  "SATURDAY"
+  "SATURDAY",
 ];
 let day = days[now.getDay()];
 thisDay.innerHTML = day;
@@ -87,7 +87,7 @@ let months = [
   "September",
   "October",
   "November",
-  "December"
+  "December",
 ];
 let month = months[now.getMonth()];
 let currentDay = now.getDate();
